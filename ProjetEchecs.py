0af127341765c96b467f7690ee1e0648f6c39130
Totole75 <anatole.parre@eleves.enpc.fr>
2016-11-28 16:49:35 +0100

    Add files via upload
    Cases accessibles

--- ProjetEchecs.py
+++ ProjetEchecs.py
@@ -40,7 +40,7 @@ class Frame(QtGui.QPushButton):
             self.setStyleSheet("background-color: white")
         else:
             self.setStyleSheet("background-color: grey")
-        self.setFixedSize(100,100)
+        self.setFixedSize(80,80)
         
     @property
     def x(self):
@@ -62,13 +62,12 @@ class Frame(QtGui.QPushButton):
             image = dicBlackPict[name]
         icon.addPixmap(QtGui.QPixmap(image),QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setIcon(icon)
-        self.setIconSize(QtCore.QSize(80, 40))
+        self.setIconSize(QtCore.QSize(100, 100))
         
 
 class Disp(QtGui.QWidget):
     def __init__(self):
         super(Disp,self).__init__()
-        
         # tableau de boutons
         self.__chessboard= [[Frame(i,j) for j in range(8)] for i in range(8)]
         mainLayout = QtGui.QGridLayout()
@@ -76,6 +75,8 @@ class Disp(QtGui.QWidget):
         
         #affichage en grille de ce tableau
         gridLayout=QtGui.QGridLayout()
+        gridLayout.setSpacing(0)
+
         for i in range(8):
             for j in range(8):
                 gridLayout.addWidget(self.__chessboard[i][j],i,j)
@@ -118,37 +119,67 @@ class Disp(QtGui.QWidget):
                     pass
                 b.setEnabled(False)
 
-    def choose_chessman(self, whiteIsPlaying):
+    def choose_chessman(self, whiteIsPlaying,tabAccess=None):
         self.unallow_all_frame()
+        #On remet l'affichage des cases accessibles au début.
+        if tabAccess != None:        
+            for (i,j) in tabAccess:
+                if (i+j)%2 == 0:
+                    self.__chessboard[i][j].setStyleSheet("background-color:\
+                                                            white")
+                else:
+                    self.__chessboard[i][j].setStyleSheet("background-color:\
+                                                                grey")
+                                                                
         for (i, j) in self.__grid.list_chessman_col(whiteIsPlaying):
             self.__chessboard[i][j].setEnabled(True)
             self.__chessboard[i][j].clicked.connect(lambda : self.allow_moves(\
-                                                        whiteIsPlaying))
+                                            whiteIsPlaying,tabAccess))
     
-    def allow_moves(self, whiteIsPlaying):
+    def allow_moves(self, whiteIsPlaying,tabAccess=None):
+        
         self.unallow_all_frame()
         fr = self.sender()
+        chessman = self.__grid[(fr.x,fr.y)] 
+        #On colore les cases accessibles (selon qu'elles sont noires ou blanches)
+        tabAccess=chessman.allowed_moves(self.__grid)
+        for (i,j) in tabAccess:
+            if (i+j)%2 == 0:
+                self.__chessboard[i][j].setStyleSheet("background-color: \
+                                                    rgb(150, 200, 150,50);")
+            else:
+                self.__chessboard[i][j].setStyleSheet("background-color: \
+                                                    rgb(50, 10, 50,100);")
+        
         # si on reclique sur la case où on est,
         # on revient a l'étape de selection d'une piece a jouer
         self.__chessboard[fr.x][fr.y].setEnabled(True)
         
         self.__chessboard[fr.x][fr.y].clicked.connect(\
-                         lambda : self.choose_chessman(whiteIsPlaying))
+                      lambda : self.choose_chessman(whiteIsPlaying,tabAccess))
 
         # si on clique sur une case on effectue donc un coup
         moves = self.__grid[(fr.x,fr.y)].allowed_moves(self.__grid)
         for (i, j) in moves:
             self.__chessboard[i][j].setEnabled(True)
             self.__chessboard[i][j].clicked.connect(lambda : \
-                                        self.play(whiteIsPlaying,fr))                                                             
+                                        self.play(whiteIsPlaying,fr,tabAccess))                                                             
         
-    def play(self, whiteIsPlaying, chessmanFrame):
+    def play(self, whiteIsPlaying, chessmanFrame,tabAccess):
         # le joueur vient de cliquer sur la case où il veut aller        
         aim = self.sender()
         
         # on recupere la piece qui va bouger
-        chessman = self.__grid[(chessmanFrame.x,chessmanFrame.y)]
-        
+        chessman = self.__grid[(chessmanFrame.x,chessmanFrame.y)] 
+       
+        #On réinitinalise les cases accessibles
+        for (i,j) in tabAccess:
+            if (i+j)%2 == 0:
+                self.__chessboard[i][j].setStyleSheet("background-color:\
+                                                        white")
+            else:
+                self.__chessboard[i][j].setStyleSheet("background-color: grey")        
+
         # on effectue le "vrai" deplacement dans self.__grid
         self.__chessboard[chessmanFrame.x][chessmanFrame.y].deleteChessMan()
         self.__grid.move((aim.x, aim.y), chessman)
