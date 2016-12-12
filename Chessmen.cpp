3a04d3a059b4a2e119937ed36c2f7bca98d35c25
Bastien0 <bastien.rolland@eleves.enpc.fr>
2016-12-12 16:19:09 +0100

    Add files via upload

--- Chessmen.cpp
+++ Chessmen.cpp
@@ -13,13 +13,13 @@ Chessman(int a, int b, string n){
 
 // deplacement dans la direction (a,b) sur
 // une ligne ou une diagonale entieres.
-vector<vector<2,int>> Chessman::move_straight(Grid grid, int a, int b){
-    vector<vector<2,int>> tabAccess;
+vector<int[2]> Chessman::move_straight(Grid grid, int a, int b){
+    vector<int[2]> tabAccess;
     int x0 = x + a;
     int y0 = y + b;
     int[] coord = {x0, y0};
     while ((0 <= x0 < 8) && (0 <= y0 < 8) && grid.isVoid(x0, y0)){
-        vector<2,int> point;
+        vector<int[2]> point;
         point[0] = x0;
         point[1] = y0;
         tabAccess.push_back(point);
@@ -28,7 +28,7 @@ vector<vector<2,int>> Chessman::move_straight(Grid grid, int a, int b){
     }
     // prise
     if (0 <=x < 8 && 0 <=y < 8 && !grid.sameColor(this, x0, y0)){
-        vector<2,int> point;
+        vector<int[2]> point;
         point[0] = x0;
         point[1] = y0;
         tabAcces.push_back(point);
@@ -39,8 +39,8 @@ vector<vector<2,int>> Chessman::move_straight(Grid grid, int a, int b){
 
 // Cette fonction renvoie une liste de positions accessibles parmi celles
 // proposées (ne marche pas pour le fou, la dame, et la tour)
-vector<vector<2,int>> Chessman::testedTuples(Grid grid, vector<vector<2,int>>  tab){
-   vector<vector<2,int>> tabAccess;
+vector<int[2]> Chessman::testedTuples(Grid grid, vector<vector<2,int>>  tab){
+   vector<int[2]> tabAccess;
    int s = tab.size();
    for (int i = 0; i < s; i++){
        point = tab[i];
@@ -54,9 +54,9 @@ vector<vector<2,int>> Chessman::testedTuples(Grid grid, vector<vector<2,int>>  t
    return tabAccess;
 }
 
-vector<vector<2,int>> Chessman::allowed_moves(Grid grid){
-    vector<vector<2,int> allowed;
-    vector<vector<2,int> vecMoves = moves(grid);
+vector<int[2]> Chessman::allowed_moves(Grid grid){
+    vector<int[2]> allowed;
+    vector<int[2]> vecMoves = moves(grid);
     for (int i = 0; i < vecMoves.size(); i++){
         if (!grid.isChessed(this, vecMoves[i][0], vecMoves[i][1]))
             allowed.push_back(vecMoves[i]);
@@ -65,21 +65,21 @@ vector<vector<2,int>> Chessman::allowed_moves(Grid grid){
 }
 
 vector<vector<2,int>> Rook::moves(Grid grid){
-    vector<vector<2,int>>  m = move_straight(grid, -1,0);
-    vector<vector<2,int>>  m1 = move_straight(grid, 1,0);
-    vector<vector<2,int>>  m2 = move_straight(grid, 0,1);
-    vector<vector<2,int>>  m3 = move_straight(grid, 0,-1);
+    vector<int[2]>  m = move_straight(grid, -1,0);
+    vector<int[2]>  m1 = move_straight(grid, 1,0);
+    vector<int[2]>  m2 = move_straight(grid, 0,1);
+    vector<int[2]>  m3 = move_straight(grid, 0,-1);
     m.insert(m.end(), m1.begin(), m1.end());
     m.insert(m.end(), m2.begin(), m2.end());
     m.insert(m.end(), m3.begin(), m3.end());
     return m;
 }
 
-vector<vector<2,int>> Bishop::moves(Grid grid){
-    vector<vector<2,int>>  m = move_straight(grid, -1,-1);
-    vector<vector<2,int>>  m1 = move_straight(grid, -1,1);
-    vector<vector<2,int>>  m2 = move_straight(grid, 1,-1);
-    vector<vector<2,int>>  m3 = move_straight(grid, 1,1);
+vector<int[2]> Bishop::moves(Grid grid){
+    vector<int[2]>  m = move_straight(grid, -1,-1);
+    vector<int[2]>  m1 = move_straight(grid, -1,1);
+    vector<int[2]>  m2 = move_straight(grid, 1,-1);
+    vector<int[2]>  m3 = move_straight(grid, 1,1);
     m.insert(m.end(), m1.begin(), m1.end());
     m.insert(m.end(), m2.begin(), m2.end());
     m.insert(m.end(), m3.begin(), m3.end());
@@ -87,15 +87,15 @@ vector<vector<2,int>> Bishop::moves(Grid grid){
 }
 
 
-vector<vector<2,int>> Queen::moves(Grid grid){
-    vector<vector<2,int>>  m = move_straight(grid, -1,-1);
-    vector<vector<2,int>>  m1 = move_straight(grid, -1,1);
-    vector<vector<2,int>>  m2 = move_straight(grid, 1,-1);
-    vector<vector<2,int>>  m3 = move_straight(grid, 1,1);
-    vector<vector<2,int>>  m4 = move_straight(grid, -1,0);
-    vector<vector<2,int>>  m5 = move_straight(grid, 1,0);
-    vector<vector<2,int>>  m6 = move_straight(grid, 0,1);
-    vector<vector<2,int>>  m7 = move_straight(grid, 0,-1);
+vector<int[2]> Queen::moves(Grid grid){
+    vector<int[2]>  m = move_straight(grid, -1,-1);
+    vector<int[2]>  m1 = move_straight(grid, -1,1);
+    vector<int[2]>  m2 = move_straight(grid, 1,-1);
+    vector<int[2]>  m3 = move_straight(grid, 1,1);
+    vector<int[2]>  m4 = move_straight(grid, -1,0);
+    vector<int[2]>  m5 = move_straight(grid, 1,0);
+    vector<int[2]>  m6 = move_straight(grid, 0,1);
+    vector<int[2]>  m7 = move_straight(grid, 0,-1);
     m.insert(m.end(), m1.begin(), m1.end());
     m.insert(m.end(), m2.begin(), m2.end());
     m.insert(m.end(), m3.begin(), m3.end());
@@ -107,13 +107,13 @@ vector<vector<2,int>> Queen::moves(Grid grid){
 }
 
 
-vector<vector<2,int>> Knight::moves(Grid grid){
-    vector<vector<2,int>> m;
+vector<int[2]> Knight::moves(Grid grid){
+    vector<int[2]> m;
     int t[4] = {-1, 1, -2, 2};
     for (int i = 0; i < 3; i++){
         for (int j = 0; j <3; j++){
             if (abs(t[i]) != abs(t[j])){
-                vector<2,int> pos;
+                int[2] pos;
                 pos[0] = x + t[i];
                 pos[1] = y + t[j];
                 m.push_back(pos);
@@ -123,12 +123,12 @@ vector<vector<2,int>> Knight::moves(Grid grid){
     return testedTuples(grid, m);
 }
 
-vector<vector<2,int>> King::moves(Grid grid){
-    vector<vector<2,int>> m;
+vector<int[2]> King::moves(Grid grid){
+    vector<int[2]> m;
     for (int i = -1; i< 2; i++){
         for (int j = -1; j< 2; j++){
             if ((i != 0) || (j != 0)){
-                vector<2,int> point;
+                int[2] point;
                 point[0] = x + i;
                 point[1] = y + j;
                 m.push_back(point);
@@ -146,7 +146,7 @@ vector<vector<2,int>> King::moves(Grid grid){
                      && !grid(i,0).hasMoved() && grid.isVoid(i, 1) && grid.isVoid(i, 3) \
                      && !grid.isChessed(this, i, 2) && !grid.isChessed(this, i, 2) \
                      && !grid.isChessed(this, i, 4) ){
-                    vector<2,int> point;
+                    int[2] point;
                     point[0] = i;
                     point[1] = 2;
                     m.push_back(point);
@@ -187,7 +187,7 @@ vector<vector<2,int>> Pawn::moves(Grid grid){
     // d'un cote
     if ( y()+ 1 < 8 && !grid.isVoid(x() + direction, y() + 1) \
         && !grid.sameColor(this, x() + direction, y() + 1)){
-            vector<2,int> point;
+            int[2] point;
             point[0] = x() + direction;
             point[1] = y() + 1;
             m.push_back(point);
@@ -195,25 +195,47 @@ vector<vector<2,int>> Pawn::moves(Grid grid){
     // de l'autre
     if ( y() - 1 >= 0 && !grid.isVoid(x() + direction, y() - 1) \
             && !grid.sameColor(this, x() + direction, y() - 1)){
-                vector<2,int> point;
+                int[2] point;
                 point[0] = x() + direction;
                 point[1] = y() - 1;
                 m.push_back(point);
     }
     // avancee de deux cases
     if (isWhite() && x() == 1 && grid.isVoid(2, y()) && grid.isVoid(3, y())){
-        vector<2,int> point;
+        int[2] point;
         point[0] = 3;
         point[1] = y();
         m.push_back(point);
     }
     if (!isWhite() && x() == 6 && grid.isVoid(5, y()) && grid.isVoid(4, y())){
-        vector<2,int> point;
+        int[2] point;
         point[0] = 4;
         point[1] = y();
         m.push_back(point);
     }
     // prise en passant
+    int P[2];
+        int Point[2];
+        P[0]=x;
+        P[1]=y+1;
+        //d'un côté
+        if (y()+ 1 < 8 && !grid.isVoid(P[0],P[1])
+                && grid(P)->name() == "Pawn" && grid(P)->isWhite() != isWhite
+                && grid(P)->double_done && grid.isVoid(P[0]+direction,P[1])){
+            Point[0]=x+direction;
+            Point[1]=y+1;
+            m.push_back(Point);
+        }
+        //de l'autre
+        P[0]=x;
+        P[1]=y-1;
+        if (y()- 1 >= 0 && !grid.isVoid(P[0],P[1])
+                && grid(P)->name() == "Pawn" && grid(P)->isWhite() != isWhite
+                && grid(P)->double_done && grid.isVoid(P[0]+direction,P[1])){
+            Point[0]=x+direction;
+            Point[1]=y-1;
+            m.push_back(Point);
+        }
 
 
 }
